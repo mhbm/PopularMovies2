a10e675edb504874b10bb3fa248a5b9006c6a271
mateus.macedo <mhbassomacedo@gmail.com>
2017-09-29 17:19:16 -0300

    Fix some bugs
    Signed-off-by: mateus.macedo <mhbassomacedo@gmail.com>

--- app/src/main/java/com/example/android/popularmovies/data/ReviewModel.java
+++ app/src/main/java/com/example/android/popularmovies/data/ReviewModel.java
@@ -1,16 +1,18 @@
 package com.example.android.popularmovies.data;
 
-import java.io.Serializable;
+import android.os.Parcel;
+import android.os.Parcelable;
 
 /**
  * Created by Mateus Macedo on 22/09/17.
  */
 
-public class ReviewModel implements Serializable {
+public class ReviewModel implements Parcelable {
     private int id;
     private String author;
     private String content;
     private String url;
+
 
     public int getId() {
         return id;
@@ -43,4 +45,39 @@ public class ReviewModel implements Serializable {
     public void setUrl(String url) {
         this.url = url;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(this.id);
+        dest.writeString(this.author);
+        dest.writeString(this.content);
+        dest.writeString(this.url);
+    }
+
+    public ReviewModel() {
+    }
+
+    protected ReviewModel(Parcel in) {
+        this.id = in.readInt();
+        this.author = in.readString();
+        this.content = in.readString();
+        this.url = in.readString();
+    }
+
+    public static final Parcelable.Creator<ReviewModel> CREATOR = new Parcelable.Creator<ReviewModel>() {
+        @Override
+        public ReviewModel createFromParcel(Parcel source) {
+            return new ReviewModel(source);
+        }
+
+        @Override
+        public ReviewModel[] newArray(int size) {
+            return new ReviewModel[size];
+        }
+    };
 }
