92eb48c61cf3b5512cd5f0db7db1aad32d370898
mateus.macedo <mhbassomacedo@gmail.com>
2017-09-29 15:29:00 -0300

    Fix some bugs
    Signed-off-by: mateus.macedo <mhbassomacedo@gmail.com>

--- app/src/main/java/com/example/android/popularmovies/adapter/VideoAdapter.java
+++ app/src/main/java/com/example/android/popularmovies/adapter/VideoAdapter.java
@@ -28,6 +28,8 @@ public class VideoAdapter extends RecyclerView.Adapter<VideoAdapter.NumberViewHo
     private ArrayList<String> frameVideo;
     private Context mContext;
 
+    private View viewVideo;
+
     public VideoAdapter(MovieModel movie, Context context) {
         mNumberItems = movie.getKeysVideos().size();
         mMovie = movie;
@@ -41,8 +43,8 @@ public class VideoAdapter extends RecyclerView.Adapter<VideoAdapter.NumberViewHo
         int layoutIdForListItem = R.layout.movie_list_video;
         LayoutInflater inflater = LayoutInflater.from(context);
         boolean shouldAttachToParentImmediately = false;
-        View view = inflater.inflate(layoutIdForListItem, viewGroup, shouldAttachToParentImmediately);
-        NumberViewHolder viewHolder = new NumberViewHolder(view);
+        viewVideo = inflater.inflate(layoutIdForListItem, viewGroup, shouldAttachToParentImmediately);
+        NumberViewHolder viewHolder = new NumberViewHolder(viewVideo);
         return viewHolder;
     }
 
@@ -64,29 +66,21 @@ public class VideoAdapter extends RecyclerView.Adapter<VideoAdapter.NumberViewHo
 
         public NumberViewHolder(View itemView) {
             super(itemView);
-            displayYoutubeVideo = (ImageButton) itemView.findViewById(R.id.ib_movie_trailer);
-            displayYoutubeName = (TextView) itemView.findViewById(R.id.tv_trailer);
+            displayYoutubeVideo = itemView.findViewById(R.id.ib_movie_trailer);
+            displayYoutubeName = itemView.findViewById(R.id.tv_trailer);
         }
 
         void bind(final int listIndex) {
 
-//            String trailerYoutube = "https://www.youtube.com/embed/" + mMovie.getKeysVideos().get(listIndex);
-//            String frameVideo = "<html><iframe width=\"match_parent\" height=\"match_parent\" src=\"" + trailerYoutube + "\" frameborder=\"0\" allowfullscreen></iframe></body></html>";
-//            WebSettings webSettings = displayYoutubeVideo.getSettings();
-//            webSettings.setJavaScriptEnabled(true);
-//            displayYoutubeVideo.loadData(frameVideo, "text/html", "utf-8");
-//            displayYoutubeVideo.setVisibility(View.VISIBLE);
+            displayYoutubeName.setText("Trailer : " + (listIndex + 1));
 
-            displayYoutubeName.setText("Trailer " + (listIndex + 1));
-            itemView.setOnClickListener(new View.OnClickListener() {
+            displayYoutubeVideo.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
 
                     Intent appIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube:" + mMovie.getKeysVideos().get(listIndex)));
                     Intent webIntent = new Intent(Intent.ACTION_VIEW,
                             Uri.parse("http://www.youtube.com/watch?v=" + mMovie.getKeysVideos().get(listIndex)));
-
-
                     try {
 
                         mContext.startActivity(appIntent);
